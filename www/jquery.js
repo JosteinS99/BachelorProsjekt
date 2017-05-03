13b30dee93c910057a48e1940fc9723394027c51
kojarB <kojar@live.no>
2017-05-03 13:09:31 +0200

    changed index and jquery.js

--- www/jquery.js
+++ www/jquery.js
@@ -1,63 +1,55 @@
 $(document).ready(function() {
-    // console.log("ready!");
-    //   $('#succesfulUpload').hide();
- // $(".delete").click(function(event){
- // 	$("th, tr").fadeOut(500, function () {
- // 		$(this).remove();
- // 	});
- // 	event.stopPropagation();
- // });
   
+function dataFunction () {
+   var cat = $("#select").val();
+   var not = $("#note").val();
+}
+ 
+ $("#upload").click(function(event){
+  console.log("I was clicked");
+  });
 
-//   <div id="box">
-// </div>
+  $("#send").click(function(event){
+dataFunction();
+  
+     var m = not;
 
-// <button id="addDiv">Add div Inside #box</button>
+    console.log(m);
+  });
 
 $(document).on('click', '#save', function(e) {
    var randomId = Math.random().toString(32).slice(2);
 
-   var test =("<tr class='rowbox' id='id"+randomId+"'><th class='textbox'><input type='text' placeholder='MM/DD/YYYY' class='input'></th><th class='buttons'><span class='material-icons succesfulUpload'>check_circle</span><span name='"+randomId+"' class='material-icons delete' id='delete'>delete</span></th></tr>");
+   var test =("<tr class='rowbox' id='id"+randomId+"'><th class='textbox'><input id='"+randomId+"' type='date' class='input' disabled></th><th class='buttons'><span class='material-icons succesfulUpload'>check_circle</span><span name='"+randomId+"' class='material-icons delete' id='delete'>delete</span></th></tr>");
     $( "#box" ).append(test);
+
+    var n = new Date();
+      var year = n.getFullYear();
+       var month = n.getMonth() + 1;
+       var day = n.getDate();
+       
+    if(month < 10) {
+    month = "0" + month;
+  } 
+  if (day < 10) {
+    day = "0" + day;
+  }
+  var today = year + "-" + month + "-" + day;
+  document.getElementById(randomId).value =today;
+   
+
  //  $( "#box" ).append( "<tr class='rowbox' id='id"+randomId+"'><button id='addDiv'>Add div Inside #box ("+randomId+")</button>   #  some cool stuff #  <button href='#' id='delete' name='"+randomId+"'>remove ("+randomId+")</button></tr>");
 });
 
 $(document).on('click', '#delete', function(e) {
    var thisRowbox = "#id" + this.attributes["name"].value;
    console.log("removing: " + thisRowbox);
-   $(thisRowbox).remove();
+  $(thisRowbox).fadeOut(500, function () {
+    $(this).remove();
+  });
+
 });
 
-  // $(thisRowbox).fadeOut(500, function () {
-  //   $(this).remove();
-  // });
-
-                 
-
-  // DETTE SKAL BRUKES TIL SAVE KNAPPEN PÅ SEND SIDEN
-// $("input[type='text']").keypress(function(event){
-// 	console.log("Key Press!");
-// 	if(event.which === 13){
-// 		// console.log("You hit enter");
-//   //henter ny notat fra textboxen"
-// 		var noteText = $(this).val();
-// 	// lag en ny tr og legg til lagringslista(table)
-// 		}
-
-//     var cloneCount = 1;
-// 		var randomcount = 1;
-// 		$(".rowbox").clone().appendTo("#box").attr('id', 'newID'+ cloneCount++).insertAfter("#id");
-// 		$(".delete").attr('id', 'id'+ randomcount++).insertAfter("#del");
-	 
-
-//   });
 
 }); //end of jquery
 
-// css( "background-color", "blue")
-// husk at input feltet er ready only, slett det for å teste db queries
-
-  // var cloneCount = 1;
-  //  $("button").click(function(){
-  //     $("#id").clone().attr('id', 'id'+ cloneCount++).insertAfter("#id");
-  //  }); 
--- www/js/my-app.js
+++ www/js/my-app.js
@@ -50,6 +50,24 @@ if (page.name === 'index') {
 
 // };
 
+function setDate() {
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+ 
+
+
+  if(month < 10) {
+    month = "0" + month;
+  } 
+  if (day < 10) {
+    day = "0" + day;
+  }
+  var today = year + "-" + month + "-" + day;
+  document.getElementById('datobox').value =today;
+}
+
 
 
 // Callbacks to run specific code for specific pages, for example for About page:
