f5b7675b0ac2edb9568cb74188c59a04509fb97a
cqb <c.bui@hotmail.no>
2017-05-16 15:16:41 +0200

    prettify koden

---
+++ frontend.js
@@ -0,0 +1,31 @@
+
+/*function callAnothePage(){
+     window.location = "secondPage.html";
+}
+
+              */
+function BackKeyDown(){
+     window.location = "index.html";
+                // navigator.notification.alert();
+                 //navigator.app.exitApp();  // For Exit Application
+}
+
+
+function setDate() {
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+ 
+
+
+  if(month < 10) {
+    month = "0" + month;
+  } 
+  if (day < 10) {
+    day = "0" + day;
+  }
+  var today = year + "-" + month + "-" + day;
+  document.getElementById('myDate').value =today;
+}
+
